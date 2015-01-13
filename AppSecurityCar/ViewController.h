5fdb460a64281dc232c530ee32e210783da84f5f
DroidFx <Drago@MacBook-de-DroidFx.local>
2015-01-12 22:52:35 -0600

    Initial Commit

---
+++ AppSecurityCar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppSecurityCar
+//
+//  Created by DroidFx on 1/12/15.
+//  Copyright (c) 2015 DroidFx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AppSecurityCarTests/AppSecurityCarTests.m
@@ -0,0 +1,40 @@
+//
+//  AppSecurityCarTests.m
+//  AppSecurityCarTests
+//
+//  Created by DroidFx on 1/12/15.
+//  Copyright (c) 2015 DroidFx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AppSecurityCarTests : XCTestCase
+
+@end
+
+@implementation AppSecurityCarTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
