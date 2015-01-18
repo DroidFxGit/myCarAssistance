1827ecbccb911c002a7d271c796bb72ddc30b332
DroidFx <Drago@MacBook-de-DroidFx.local>
2015-01-17 23:57:41 -0600

    first master changes

--- AppSecurityCar/FirstViewController.m
+++ AppSecurityCar/FirstViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2015 DroidFx. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "FirstViewController.h"
 
-@interface ViewController ()
+@interface FirstViewController ()
 
 @end
 
-@implementation ViewController
+@implementation FirstViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
---
+++ AppSecurityCar/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  AppSecurityCar
+//
+//  Created by DroidFx on 1/12/15.
+//  Copyright (c) 2015 DroidFx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
